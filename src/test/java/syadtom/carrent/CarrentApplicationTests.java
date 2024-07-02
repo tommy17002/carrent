2345d4bfa0d3de9e125f6dab469a113e10e48418
irsyad-hash <irsyadfauzi2345@gmail.com>
2024-07-02 10:20:57 +0700

    Add files via upload

---
+++ src/test/java/syadtom/carrent/CarrentApplicationTests.java
@@ -0,0 +1,13 @@
+package syadtom.carrent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
